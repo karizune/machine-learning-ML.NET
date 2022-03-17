5901e07e8013bea4e9cfb8a21e1e2de060640dcb
karizune <luanchrystian2@gmail.com>
2022-03-17 15:23:11 -0300

    first commit

---
+++ Machine learning csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Machine_learning_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
